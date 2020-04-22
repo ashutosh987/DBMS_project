82839e3f9fdc207092d5d630c44526388bec9fe9
alok garg <alk.grg7@gmail.com>
2020-04-22 10:07:46 +0530

    added agent routes

--- routes/connection.js
+++ routes/connection.js
@@ -1,10 +1,10 @@
 require('dotenv').config();
 var mysql=require('mysql');
 var mysqlConnection=mysql.createConnection({
-    host: "localhost",
-  user: "root",
-  password: "alokgarg#01",
-  database: "dbms",
+    host:process.env.DB_HOST,
+    user:process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database:process.env.MYSQL_DB,
     multipleStatements:true
 });
 
