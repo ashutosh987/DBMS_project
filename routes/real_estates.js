5e15c81b61673b71ba6ea65690d3a5ed7ab7880e
ashutosh987 <guptaashutosh427@gmail.com>
2020-04-23 16:11:15 +0530

    asd

--- routes/real_estates.js
+++ routes/real_estates.js
@@ -15,5 +15,52 @@ router.get("/",(req,res)=>{
 			res.send('nothing for you');
 		}
 	});
-    });
+	});
+	
+	router.post("/search",(req,res)=>{
+	var city=req.body.city;
+	var address1=req.body.amount1;
+	var address2=req.body.amount2;
+		if(city&&address1){
+			myconnection.query('select * from property where city=? and price between ? and ?',[city,address1,address2],function (error,results) {
+				if(results)	{	
+					res.render("search",{results:results});
+				}
+				else{
+					res.send('nothing for you');
+				} 
+			});	
+
+		}
+		else if(city){
+			myconnection.query('select * from property where city=?',[city],function (error,results) {
+				if(results)	{	
+				//res.redirect('/agent/'+agent_id+'/profile');
+				res.render("search",{results:results});
+				}
+				else{
+					res.send('nothing for you');
+				} 
+			});	
+
+		}
+		else{
+			myconnection.query('select * from property where   price between ? and ?',[address1,address2],function (error,results) {
+				if(results)	{	
+					//res.redirect('/agent/'+agent_id+'/profile');
+					res.render("search",{results:results});
+				}
+				else{
+					res.send('nothing for you');
+				} 
+			});	
+
+		}
+
+	
+	})
+
+
+
+
     module.exports=router;
